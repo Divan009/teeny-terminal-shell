74b6d1922af38d2187cfc1723bf68c96d8762979
Diva D <divadugar@gmail.com>
2025-09-17 15:58:08 +0530

    refactored and added type

--- app/main.py
+++ app/main.py
@@ -1,17 +1,52 @@
 import sys
 
 
+class Command:
+    def run(self, args: str):
+        raise NotImplementedError
+
+
+class Echo(Command):
+    def run(self, args: str):
+        print(args)
+
+
+class Exit(Command):
+    def run(self, args: str):
+        sys.exit(0)
+
+
+class Type(Command):
+    def __init__(self, registry):
+        self.registry = registry
+
+    def run(self, args):
+        if args in self.registry:
+            print(f"{args} is a shell builtin command")
+        else:
+            print(f"{args}: not found")
+
+
 def main():
     # Wait for user input
+    registry = {
+        "echo": Echo(),
+        "exit": Exit(),
+    }
+    # making this a key in register
+    registry["type"] = Type(registry)
+
     while True:
-        command = input("$ ")
-        split_cmd = command.strip().split(" ", 1)
-        if split_cmd[0] == "echo":
-            print(split_cmd[-1])
-        elif split_cmd[0] == "exit":
-            sys.exit(0)
+        command = input("$ ").strip()
+        if not command:
+            continue
+        cmd, *rest = command.split(" ", 1)
+        args = rest[0] if rest else ""
+
+        if cmd in registry:
+            registry[cmd].run(args)
         else:
-            print(f"{command}: command not found")
+            print(f"{cmd}: command not found")
 
 
 if __name__ == "__main__":
